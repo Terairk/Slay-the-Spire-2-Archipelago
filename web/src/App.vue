1a9afd9b13c70b3ae7b3acd7e515f40cbcd6271e
Douglas Lueben <dlueben1@umbc.edu>
2026-08-02 10:30:21 -0400

    docs: install nuxt ui

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,5 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <UApp>
+    <b>Hey</b>
+  </UApp>
 </template>
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import ui from "@nuxt/ui/vue-plugin";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(ui);
+
+app.mount("#app");
