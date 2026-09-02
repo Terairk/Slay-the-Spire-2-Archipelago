44cdeace3aa9b3b4ce6b4556d8e8fedd4f38540a
Terairk <terairkgaming@gmail.com>
2026-09-02 12:15:28 +0800

    Correct option descriptions

--- world/spire2/web_world.py
+++ world/spire2/web_world.py
@@ -1,8 +1,8 @@
 from BaseClasses import Tutorial
 from worlds.AutoWorld import WebWorld
-from Options import DeathLink, OptionGroup
+from Options import OptionGroup
 from .options import (
-    Characters, ModdedCharacters, DeathLinkDamagePercent, EnableDeathFragments, PickNumberCharacters, GoalNumChar,
+    Characters, ModdedCharacters, DeathLink, DeathLinkDamagePercent, EnableDeathFragments, PickNumberCharacters, GoalNumChar,
     LockCharacters, UnlockedCharacter, Ascension, AscensionDown, AdvancedChar, CharacterOptions,
     IncludeFloorChecks, NeowSanity, CampfireSanity, GoldSanity, PotionSanity,
     ShopSanity, ShopCardSlots, ShopNeutralSlots, ShopRelicSlots, ShopPotionSlots, ShopRemoveSlots, ShopSanityCosts,
