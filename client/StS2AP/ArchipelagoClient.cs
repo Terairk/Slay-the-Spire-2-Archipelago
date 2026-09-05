35e6a901b636840a671937e177464491861bcf05
Terairk <terairkgaming@gmail.com>
2026-09-05 11:40:48 +0800

    Revert "Use the client manifest as the mod version authority"
    This reverts commit a4f19497d10c7623a5749662cee8188475a74feb.

--- client/StS2AP/ArchipelagoClient.cs
+++ client/StS2AP/ArchipelagoClient.cs
@@ -8,7 +8,6 @@ using Godot;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Models;
 using Newtonsoft.Json.Linq;
-using System.Text.Json;
 using StS2AP.Data;
 using StS2AP.Extensions;
 using StS2AP.Models;
@@ -37,10 +36,6 @@ namespace StS2AP
     /// </summary>
     public static class ArchipelagoClient
     {
-        private const string ModManifestResourceName = "StS2AP.Archipelago.json";
-        private static readonly Lazy<System.Version> ModManifestVersion =
-            new(ReadModManifestVersion);
-
         /// <summary>
         /// The version of the Archipelago Mod (semantic version: major.minor.patch)
         /// </summary>
@@ -48,32 +43,11 @@ namespace StS2AP
         {
             get
             {
-                System.Version version = ModManifestVersion.Value;
+                var version = typeof(ArchipelagoClient).Assembly.GetName().Version;
+                if (version == null)
+                    return "Version Unknown";
                 return $"v{version.Major}.{version.Minor}.{version.Build}";
             }
-        }
-
-        private static System.Version ReadModManifestVersion()
-        {
-            using Stream stream = typeof(ArchipelagoClient).Assembly.GetManifestResourceStream(
-                ModManifestResourceName
-            ) ?? throw new InvalidDataException(
-                $"Embedded mod manifest '{ModManifestResourceName}' was not found."
-            );
-            using JsonDocument manifest = JsonDocument.Parse(stream);
-            if (!manifest.RootElement.TryGetProperty("version", out JsonElement versionElement))
-                throw new InvalidDataException("Embedded mod manifest has no version field.");
-
-            string? versionText = versionElement.GetString();
-            string semanticCore = versionText?.Split('-', '+')[0] ?? string.Empty;
-            if (!System.Version.TryParse(semanticCore, out System.Version? version)
-                || version.Build < 0)
-            {
-                throw new InvalidDataException(
-                    $"Embedded mod manifest version '{versionText}' is not semantic X.Y.Z."
-                );
-            }
-            return version;
         }
 
         #region Connection Info
